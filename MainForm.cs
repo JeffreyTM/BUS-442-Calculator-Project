adb311cf487221cc236b0fdb713228666b6de9af
Visio <Visio@JEFFREY-XPS15>
2022-03-30 21:21:31 -0400

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CalculatorProject
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void reciprocalButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fibonacciButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            //Add the verification code later
+        }
+
+    }
+}
